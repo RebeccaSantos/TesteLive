c91d8f19edf32556368167438e7fbba64684ec0f
Sthefany <sthefanycristovam@gmail.com>
2020-07-07 21:08:58 -0300

    Modificando: request adicionado

---
+++ Models/Request/PersonagemRequest.cs
@@ -0,0 +1,9 @@
+namespace TesteLive.Models.Request
+{
+    public class PersonagemRequest
+    {
+        public string Personagem { get; set; }
+        public string Filme { get; set; }
+        public string Ator { get; set; }
+    }
+}
